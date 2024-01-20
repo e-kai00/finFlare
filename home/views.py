ec6f492fc77214340c3a449ab1ebae4f0033f054
Katerina <yelkaterina@gmail.com>
2024-01-20 22:53:12 +0100

    Set up toast messages

--- home/views.py
+++ home/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
+
 
 def home_view(request):
+    # messages.success(request, 'This is a test success message.')
+    messages.warning(request, 'This is a warning message.')
+    messages.error(request, 'This is an error message.')
+    messages.info(request, 'This is an info message.')
     return render(request, 'home/index.html')
