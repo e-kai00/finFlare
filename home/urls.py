b0d9034f8b805fa5f2b5327666ba0deb89242431
Kim Bergstrom <kimmen_b@hotmail.com>
2024-01-21 17:23:04 +0000

    add: privacy and TOS html templates with simple content

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from .views import home_view
+from django.contrib import admin
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', views.home_view, name='home'),
+    path('privacy/', views.privacy_view, name='privacy'),
+    path('terms_of_service/', views.terms_of_service_view, name='terms_of_service'),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,11 @@ from django.contrib import messages
 
 def home_view(request):    
     return render(request, 'home/index.html')
+
+
+def privacy_view(request):    
+    return render(request, 'home/privacy.html')
+
+
+def terms_of_service_view(request):    
+    return render(request, 'home/terms_of_service.html')
