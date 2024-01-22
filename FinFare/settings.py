5273c11dbe2b4d599d99bf19a18ec7c7b9796fd6
Kim Bergstrom <kimmen_b@hotmail.com>
2024-01-22 14:10:11 +0000

    fix: styling colelctive for heroku deployment

--- FinFare/settings.py
+++ FinFare/settings.py
@@ -20,7 +20,7 @@ API_KEY = os.environ.get("API_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [(os.environ.get('LOCALHOST')), (os.environ.get('HEROKU_HOSTNAME')), '8000-kimbergstroem-finflare-v62zu29kngw.ws-eu107.gitpod.io', '8000-sandrabergstro-finflare-fk8ms16p7zy.ws-eu107.gitpod.io', '127.0.0.1']
+ALLOWED_HOSTS = [os.environ.get('LOCALHOST'), os.environ.get('HEROKU_HOSTNAME'), '8000-kimbergstroem-finflare-v62zu29kngw.ws-eu107.gitpod.io', '8000-sandrabergstro-finflare-fk8ms16p7zy.ws-eu107.gitpod.io', '127.0.0.1']
 
 
 # Application definition
@@ -142,17 +142,10 @@ USE_I18N = True
 USE_TZ = True
 
 
-
 # Static files (CSS, JavaScript, Images)
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
-
-STATIC_URL = "/static/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
