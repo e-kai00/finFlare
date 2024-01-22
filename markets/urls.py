99e9a4fe314f80e930b1d93db750e4454c653250
Vernell Clark <vernellclark@hotmail.com>
2024-01-22 12:40:51 +0100

    Add paths to urlpatterns

--- markets/urls.py
+++ markets/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('markets/', views.stock_data, name='markets'),
     path('stock_data/<str:symbol>/', views.stock_data, name='stock_data'),
     path('trade/', views.trade_stock, name='trade_stock'),
+    path('update_balances/', views.update_balances, name='update_balances'),
+    path('get_user_balance/', views.get_user_balance, name='get_user_balance'),
+    path('get_stock_prices/', views.get_stock_prices, name='get_stock_prices'),
+    path('get_user_stock_balances/', views.get_user_stock_balances, name='get_user_stock_balances'),
 ]
