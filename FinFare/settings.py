fc77ebadaa22b079031da20b53ec566d0cd713dd
Kim Bergstrom <kimmen_b@hotmail.com>
2024-01-22 14:05:53 +0000

    fix: styling collective in heroku deployment

--- FinFare/settings.py
+++ FinFare/settings.py
@@ -147,12 +147,7 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
 STATIC_URL = "/static/"
-STATICFILES_STORAGE = (
-    "cloudinary_storage.storage.StaticHashedCloudinaryStorage"
-)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
