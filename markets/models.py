6de1588c01c8838c9d3b1b3ead30496f04faf017
Katerina <yelkaterina@gmail.com>
2024-03-08 15:50:11 +0100

    Updated database models (#135)
    * Set up to display market screen without api
    
    * Add set-up to test custom error pages
    
    * Tidy-ups
    
    * Re-define models
    
    * Drop db

--- markets/models.py
+++ markets/models.py
@@ -5,7 +5,7 @@ from accounts.models import UserAccountPortfolio
 
 class Stock(models.Model):
     """
-    Saving specific stock values to DB (coming from API)
+    Saving stock values to DB (coming from API)
     """
     symbol = models.CharField(max_length=10)
     name = models.CharField(max_length=255)
@@ -13,25 +13,48 @@ class Stock(models.Model):
     price_movement = models.CharField(max_length=10) 
 
 
+class Transaction(models.Model):
+    """
+    Allows to keep record of all transactions
+    """
+    user_profile = models.ForeignKey(UserAccountPortfolio, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=4, choices=[('BUY', 'Buy'), ('SELL', 'Sell')])
+    stock = models.ForeignKey(Stock, on_delete=models.PROTECT) 
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
 class StockBalance(models.Model):
     """
     Current Balance and purschase of User (open positions)
     """
     user = models.ForeignKey(UserAccountPortfolio, on_delete=models.CASCADE)
-    stock = models.CharField(max_length=30)
+    stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField()
-    purchase_price = models.DecimalField(max_digits=10, decimal_places=4) 
+    price = models.ForeignKey(Transaction, on_delete=models.CASCADE) 
     is_buy_position = models.BooleanField(default=True)
 
+    @property
+    def current_price(self):
+        """ retrieve current price from the associated Stock """
+        return self.stock.price
+    
 
-class Transaction(models.Model):
-    """
-    Allow keep a record of all transactions
-    """
-    user_profile = models.ForeignKey(UserAccountPortfolio, on_delete=models.CASCADE)
-    transaction_type = models.CharField(max_length=4, choices=[('BUY', 'Buy'), ('SELL', 'Sell')])
-    name = models.CharField(max_length=30)
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    @property
+    def calculate_stock_value(self):
+        """ calculate current stock balance """
+        return self.quantity * self.price.price
+
+    @property
+    def claculate_total(self):
+        """ calculate profit or loss """
+        return (self.curren_price - self.price) * self.quantity
+    
+
+
+    
+
+# drop database
+
 
